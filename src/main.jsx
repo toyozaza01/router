94b0910b16fbd26eb9e1377abf1887643483a055
Toyo Prapatsorn <toyoaz01@gmail.com>
2025-02-14 01:25:05 +0700

    No Route

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import NavBar from './components/NavBar.jsx'
+import Footer from './components/Footer.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <NavBar/>
+    <App />
+    <Footer></Footer>
+  </StrictMode>,
+)
