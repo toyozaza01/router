4d55859f1fead9623aeb9eed6ee760cde2a9734b
toyozaza01 <toyoaz01@gmail.com>
2025-02-14 12:23:58 +0700

    Edited Page

---
+++ src/components/carousels.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../css/carousels.css";
+
+export const carousels = () => {
+  return (
+    <div>
+      <div class="container">
+        <div>
+          <div class="content">
+            <h2>My Info</h2>
+            <span>Prapatsorn Ch.</span>
+          </div>
+        </div>
+        <div>
+          <div class="content">
+            <h2>My Activities</h2>
+            <span>Prapatsorn Ch.</span>
+          </div>
+        </div>
+        <div>
+          <div class="content">
+            <h2>My Education Background</h2>
+            <span>Prapatsorn Ch.</span>
+          </div>
+        </div>
+        <div>
+          <div class="content">
+            <h2>Contact us</h2>
+            <span>Prapatsorn Ch.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default carousels;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import NavBar from './components/NavBar.jsx'
-import Footer from './components/Footer.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import NavBar from "./components/NavBar.jsx";
+import Footer from "./components/Footer.jsx";
+import Carous from "./components/carousels.jsx"
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NavBar/>
-    <App />
+    <NavBar />
+    <Carous/>
+    {/* <App /> */}
     <Footer></Footer>
-  </StrictMode>,
-)
+  </StrictMode>
+);
