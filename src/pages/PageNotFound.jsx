22739a6558c226b96b80de7078365fa952d7f953
toyozaza01 <toyoaz01@gmail.com>
2025-02-14 23:47:31 +0700

    All Done

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const PageNotFound = () => {
   return (
     <>
-       <h2>Page Not Found</h2>
-       <div>There is no such a page.</div>
-       <hr />
-       <Link to='/'>Back Home</Link>
-       </>
+      <h2>Page Not Found</h2>
+      <div>There is no such a page.</div>
+      <hr />
+      <Link to="/">Back Home</Link>
+    </>
+  );
+};
 
-  )
-}
-
-export default PageNotFound
+export default PageNotFound;
